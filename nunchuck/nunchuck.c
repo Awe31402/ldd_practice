92d9d49f62e2f384486d8fa318f0a0ca1d5c55ad
edward wang <edward.wang@insyde.com>
2017-08-07 00:11:48 +0800

    [Nunchuck] add attriute files

--- nunchuck/nunchuck.c
+++ nunchuck/nunchuck.c
@@ -4,16 +4,14 @@
 #include <linux/delay.h>
 #include <linux/kthread.h>
 #include <linux/input.h>
+#include <linux/fs.h>
+#include <linux/mutex.h>
+#include <linux/sysfs.h>
 
 #define DRIVER_NAME "nunchuck"
 #define THREAD_SLEEP_MS 50
 #define JOYSTICK_SCALE_FACTOR 3
 #define JOYSTICK_IGNORE_THREASHOLD 5
-
-int joystick_scale_factor = JOYSTICK_SCALE_FACTOR;
-unsigned int thread_sleep_ms = THREAD_SLEEP_MS;
-int joystick_ignore_threshold = JOYSTICK_IGNORE_THREASHOLD;
-
 #define usleep(micro_sec) usleep_range(micro_sec, micro_sec + 500)
 
 struct task_struct *nunchuck_tsk;
@@ -35,23 +33,143 @@ struct nunchuck_signal {
 
 #define ABS(i) (((i) < 0)? (-1 * (i)) : (i))
 
+struct nunchuck_params {
+    int joystick_scale_factor;
+    unsigned int thread_sleep_ms;
+    int joystick_ignore_threshold;
+    struct mutex lock;
+};
+
+struct nunchuck_params params = {
+    .joystick_scale_factor = JOYSTICK_SCALE_FACTOR,
+    .thread_sleep_ms = THREAD_SLEEP_MS,
+    .joystick_ignore_threshold = JOYSTICK_IGNORE_THREASHOLD,
+};
+
+struct kobject attr_kobj = {
+    .name = "control",
+};
+
+static ssize_t scale_factor_show(struct device* dev,
+        struct device_attribute *attr, char* buffer)
+{
+    int scf;
+    if (mutex_lock_interruptible(&params.lock))
+        return -EIO;
+    scf = params.joystick_scale_factor;
+    mutex_unlock(&params.lock);
+    return sprintf(buffer, "%d\n", scf);
+}
+
+static ssize_t scale_factor_store(struct device* dev,
+        struct device_attribute* attr, const char* buffer, size_t count)
+{
+    int scf = -1;
+    if (mutex_lock_interruptible(&params.lock))
+        return -EIO;
+    sscanf(buffer, "%d", &scf);
+
+    if (scf == -1) {
+        mutex_unlock(&params.lock);
+        return -EINVAL;
+    }
+
+    params.joystick_scale_factor = scf;
+
+    mutex_unlock(&params.lock);
+    return count;
+}
+
+static DEVICE_ATTR(scale_factor, S_IRUGO | S_IWUSR , scale_factor_show, scale_factor_store);
+
+static ssize_t sleep_ms_show(struct device* dev,
+        struct device_attribute* attr, char* buffer)
+{
+    unsigned int sms;
+    if (mutex_lock_interruptible(&params.lock))
+        return -EIO;
+    sms = params.thread_sleep_ms;
+    mutex_unlock(&params.lock);
+    return sprintf(buffer, "%u\n", sms);
+}
+
+static ssize_t sleep_ms_store(struct device* dev,
+        struct device_attribute* attr, const char* buffer, size_t count)
+{
+    unsigned int sms = 0;
+    if (mutex_lock_interruptible(&params.lock))
+        return -EIO;
+    sscanf(buffer, "%u", &sms);
+
+    if (sms <= 0) {
+        mutex_unlock(&params.lock);
+        return -EINVAL;
+    }
+
+    params.thread_sleep_ms = sms;
+
+    mutex_unlock(&params.lock);
+    return count;
+}
+
+static DEVICE_ATTR(sleep_ms, S_IRUGO | S_IWUSR, sleep_ms_show, sleep_ms_store);
+
+static ssize_t threshold_show(struct device* dev,
+        struct device_attribute *attr, char* buffer)
+{
+    int sts;
+    if (mutex_lock_interruptible(&params.lock))
+        return -EIO;
+    sts = params.joystick_ignore_threshold;
+    mutex_unlock(&params.lock);
+    return sprintf(buffer, "%u\n", sts);
+}
+
+static ssize_t threshold_store(struct device* dev,
+        struct device_attribute* attr, const char* buffer, size_t count)
+{
+    int sts = -1;
+    if (mutex_lock_interruptible(&params.lock))
+        return -EIO;
+    sscanf(buffer, "%d", &sts);
+
+    if (sts < 0) {
+        mutex_unlock(&params.lock);
+        return -EINVAL;
+    }
+
+    params.joystick_ignore_threshold = sts;
+
+    mutex_unlock(&params.lock);
+    return count;
+}
+
+static DEVICE_ATTR(threshold, S_IRUGO | S_IWUSR, threshold_show, threshold_store);
+
+const struct attribute* nunparam_attrs[] = {
+    &dev_attr_threshold.attr,
+    &dev_attr_scale_factor.attr,
+    &dev_attr_sleep_ms.attr,
+    NULL,
+};
+
 int parse_nunchuck_signal(struct nunchuck_signal* signal,
-        char* buf, int count)
+        char* buf, int count, int scale_factor, int threshold)
 {
     if (count < 6)
         return -1;
 
-    signal->jx = (int) (buf[0] - (char) 128) / joystick_scale_factor;
-    signal->jy = (int) ((char) 128 - buf[1]) / joystick_scale_factor;
+    signal->jx = (int) (buf[0] - (char) 128) / scale_factor;
+    signal->jy = (int) ((char) 128 - buf[1]) / scale_factor;
     signal->bz = (~buf[5]) & 0x01;
     signal->bc = ((~buf[5]) >> 1) & 0x01;
     signal->ax = (buf[5] >> 2 & 0x3) | (buf[2] << 2);
     signal->ay = (buf[5] >> 4 & 0x3) | (buf[3] << 2);
     signal->az = (buf[5] >> 6 & 0x3) | (buf[4] << 2);
 
-    if (ABS(signal->jx) < joystick_ignore_threshold)
+    if (ABS(signal->jx) < threshold)
         signal->jx = 0;
-    if (ABS(signal->jy) < joystick_ignore_threshold)
+    if (ABS(signal->jy) < threshold)
         signal->jy = 0;
 
     /*printk(KERN_INFO "[%s] jx: %d jy: %d bz: %d bc: %d ax: %d ay: %d az: %d\n",
@@ -104,10 +222,23 @@ static int nunchuck_thread(void* data)
     char buf[6];
     struct nunchuck_signal sig;
 
+    int threshold;
+    unsigned int sleep_ms;
+    int scale_factor;
+
     while (!kthread_should_stop()) {
         if (nunchuck_read_registers(client, buf, sizeof(buf)) < 0)
             goto next_loop;
-        if (!parse_nunchuck_signal(&sig, buf, sizeof(buf))) {
+        if (mutex_lock_interruptible(&params.lock))
+            goto next_loop;
+
+        threshold = params.joystick_ignore_threshold;
+        sleep_ms = params.thread_sleep_ms;
+        scale_factor = params.joystick_scale_factor;
+
+        mutex_unlock(&params.lock);
+        if (!parse_nunchuck_signal(&sig, buf, sizeof(buf),
+                    scale_factor, threshold)) {
             input_report_key(input, BTN_LEFT, sig.bc);
             input_report_key(input, BTN_RIGHT, sig.bz);
             input_report_rel(input, REL_X, sig.jx);
@@ -115,7 +246,7 @@ static int nunchuck_thread(void* data)
             input_sync(input);
         }
 next_loop:
-        msleep(thread_sleep_ms);
+        msleep(params.thread_sleep_ms);
     }
 
     return 0;
@@ -171,9 +302,13 @@ static int nunchuck_probe(struct i2c_client* client,
     nunchuck->input_dev = input;
 
     result = input_register_device(nunchuck->input_dev);
-
     if (result < 0)
         goto register_err;
+
+    mutex_init(&params.lock);
+    result = sysfs_create_files(&client->dev.kobj, nunparam_attrs);
+    if (result)
+        goto create_file_error;
 
     result = nunchuck_write_registers(client, init1, sizeof(init1));
     if (result < 0)
@@ -187,7 +322,10 @@ static int nunchuck_probe(struct i2c_client* client,
 
     return 0;
 
+
 write_error:
+    sysfs_remove_files(&client->dev.kobj, nunparam_attrs);
+create_file_error:
     input_unregister_device(nunchuck->input_dev);
 register_err:
     input_free_device(nunchuck->input_dev);
@@ -203,6 +341,7 @@ static int nunchuck_remove(struct i2c_client* client)
             DRIVER_NAME, __func__, __LINE__);
 
     kthread_stop(nunchuck_tsk);
+    sysfs_remove_files(&client->dev.kobj, nunparam_attrs);
     input_unregister_device(input);
     input_free_device(input);
 
